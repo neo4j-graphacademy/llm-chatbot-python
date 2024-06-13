219bda7b77dd22ed4d9d7cb83c57855b25699b17
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2024-06-13 12:03:23 +0100

    in progress updates

---
+++ agent.py
@@ -0,0 +1,11 @@
+from llm import llm
+from graph import graph
+from utils import get_session_id
+
+# Create a movie chat chain
+
+# Create a set of tools
+
+# Create the agent
+
+# Create a handler to call the agent
--- solutions/agent.py
+++ solutions/agent.py
@@ -1,50 +1,83 @@
-# tag::importtool[]
+from llm import llm
+from graph import graph
+
+# tag::import_movie_chat[]
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+# end::import_movie_chat[]
+
+# tag::import_tool[]
 from langchain.tools import Tool
-# end::importtool[]
+# end::import_tool[]
+
+# tag::import_memory[]
+from langchain_community.chat_message_histories import Neo4jChatMessageHistory
+# end::import_memory[]
+
+# tag::import_get_session_id[]
+from utils import get_session_id
+# end::import_get_session_id[]
+
+
+# tag::import_agent[]
 from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain import hub
-# tag::importmemory[]
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-# end::importmemory[]
+# end::import_agent[]
 
-from solutions.llm import llm
+# tag::movie_chat[]
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a movie expert providing information about movies."),
+        ("human", "{input}"),
+    ]
+)
 
-# Use the Chains built in the previous lessons
-from solutions.tools.vector import kg_qa
-# from solutions.tools.fewshot import cypher_qa
-from solutions.tools.finetuned import cypher_qa
+movie_chat = chat_prompt | llm | StrOutputParser()
+# end::movie_chat[]
 
 # tag::tools[]
 tools = [
     Tool.from_function(
         name="General Chat",
-        description="For general chat not covered by other tools",
-        func=llm.invoke,
-        return_direct=True
-    ),
-    Tool.from_function(
-        name="Cypher QA",
-        description="Provide information about movies questions using Cypher",
-        func = cypher_qa,
-        return_direct=True
-    ),
-    Tool.from_function(
-        name="Vector Search Index",
-        description="Provides information about movie plots using Vector Search",
-        func = kg_qa,
-        return_direct=True
+        description="For general movie chat not covered by other tools",
+        func=movie_chat.invoke,
     )
 ]
 # end::tools[]
 
+# # Use the Chains built in the previous lessons
+# from solutions.tools.vector import kg_qa
+# # from solutions.tools.fewshot import cypher_qa
+# from solutions.tools.finetuned import cypher_qa
 
-# tag::memory[]
-memory = ConversationBufferWindowMemory(
-    memory_key='chat_history',
-    k=5,
-    return_messages=True,
-)
-# end::memory[]
+# tag::tools[]
+# tools = [
+#     Tool.from_function(
+#         name="General Chat",
+#         description="For general chat not covered by other tools",
+#         func=llm.invoke,
+#         return_direct=True
+#     ),
+#     Tool.from_function(
+#         name="Cypher QA",
+#         description="Provide information about movies questions using Cypher",
+#         func = cypher_qa,
+#         return_direct=True
+#     ),
+#     Tool.from_function(
+#         name="Vector Search Index",
+#         description="Provides information about movie plots using Vector Search",
+#         func = kg_qa,
+#         return_direct=True
+#     )
+# ]
+# end::tools[]
+
+# tag::get_memory[]
+def get_memory(session_id):
+    return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
+# end::get_memory[]
 
 # tag::agent[]
 agent_prompt = hub.pull("hwchase17/react-chat")
@@ -52,19 +85,27 @@ agent = create_react_agent(llm, tools, agent_prompt)
 agent_executor = AgentExecutor(
     agent=agent,
     tools=tools,
-    memory=memory,
     verbose=True
     )
+
+chat_agent = RunnableWithMessageHistory(
+    agent_executor,
+    get_memory,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
 # end::agent[]
 
 # tag::generate_response[]
-def generate_response(prompt):
+def generate_response(user_input):
     """
     Create a handler that calls the Conversational agent
     and returns a response to be rendered in the UI
     """
 
-    response = agent_executor.invoke({"input": prompt})
+    response = chat_agent.invoke(
+        {"input": user_input},
+        {"configurable": {"session_id": get_session_id()}},)
 
     return response['output']
 # end::generate_response[]
