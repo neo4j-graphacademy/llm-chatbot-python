2aac45d81aad959cec319f53cfbcf1d5fb151fd5
TanukiDong <europazas@hotmail.com>
2024-06-27 17:28:20 +0700

    Creating an Agent

--- agent.py
+++ agent.py
@@ -2,11 +2,66 @@ from llm import llm
 from graph import graph
 
 # Create a movie chat chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+
+chat_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a movie expert providing information about movies."),
+        ("human", "{input}"),
+    ]
+)
+
+movie_chat = chat_prompt | llm | StrOutputParser()
 
 # Create a set of tools
+from langchain.tools import Tool
+
+tools = [
+    Tool.from_function(
+        name="General Chat",
+        description="For general movie chat not covered by other tools",
+        func=movie_chat.invoke,
+    )
+]
 
 # Create chat history callback
+from langchain_community.chat_message_histories import Neo4jChatMessageHistory
+
+def get_memory(session_id):
+    return Neo4jChatMessageHistory(session_id=session_id, graph=graph)
 
 # Create the agent
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain import hub
+
+agent_prompt = hub.pull("hwchase17/react-chat")
+agent = create_react_agent(llm, tools, agent_prompt)
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=tools,
+    verbose=True
+    )
+
+chat_agent = RunnableWithMessageHistory(
+    agent_executor,
+    get_memory,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+)
 
 # Create a handler to call the agent
+from utils import get_session_id
+
+def generate_response(user_input):
+    """
+    Create a handler that calls the Conversational agent
+    and returns a response to be rendered in the UI
+    """
+
+    response = chat_agent.invoke(
+        {"input": user_input},
+        {"configurable": {"session_id": get_session_id()}},)
+
+    return response['output']
--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from utils import write_message
+from agent import generate_response
 
 # Page Config
 st.set_page_config("Ebert", page_icon=":movie_camera:")
@@ -21,11 +22,9 @@ def handle_submit(message):
 
     # Handle the response
     with st.spinner('Thinking...'):
-        # # TODO: Replace this with a call to your LLM
-        from time import sleep
-        sleep(1)
-        write_message('assistant', message)
-
+        # Call the agent
+        response = generate_response(message)
+        write_message('assistant', response)
 
 # Display messages in Session State
 for message in st.session_state.messages:
