98d8a326d5b7fcc76ff7a600b56c906e4b1d7890
jyeh2 <yehjames526@gmail.com>
2024-11-25 01:37:32 +0800

    llm.py setup

--- llm.py
+++ llm.py
@@ -1,6 +1,15 @@
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 
 # Create the LLM
-
+llm = ChatOpenAI(
+    openai_api_key=st.secrets["OPENAI_API_KEY"],
+    model=st.secrets["OPENAI_MODEL"],
+)
 
 # Create the Embedding model
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key=st.secrets["OPENAI_API_KEY"]
+)
